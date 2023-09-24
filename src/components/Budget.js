b5b9c740d5425cc98e4b2308a3ea08c98b422477
GokhanDuzel <gduze020@uottawa.ca>
2023-09-24 17:47:28 -0400

    budget allocation app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,25 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+    const increaseBudget = () => {
+        dispatch({
+          type: "SET_BUDGET",
+          payload: newBudget,
+        });
+    };
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £{budget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<button type="button" className="btn btn-info" onClick={increaseBudget}>Change budget</button>
+</div>
+    );
+};
+export default Budget;
